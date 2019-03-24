f7668e2205e9b91cc21050cdbdca6f4df071227b
Babecherry <yimei.zhou@my.jcu.edu.au>
2019-03-24 10:29:13 +0800

    add

---
+++ Exercises 1/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong
+{
+   public static void main(String[] args)
+   {
+      System.out.println("My Favorite Song: ");
+      System.out.println("Who do you love");
+      System.out.println("Animals");
+      System.out.println("SOLO");
+      System.out.println("The Saltwater Room");
+   }
+}
---
+++ Exercises 1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo
+{
+   public static void main(String[] args)
+   {
+      System.out.println("My Favorite Movie: Transformers");
+      System.out.println("Movie Quote: Cade Yeager: I think we just found a Transformer.");
+      System.out.println("Characters: Optimus Prime, Bumblebee, Prowl");
+      System.out.println("Year: 2007");
+   }
+}
